501d2185a6c64fbcb7f5fa88b36b7226f3621296
Camila Moreno <cam.morenop@duocuc.cl>
2025-04-07 23:06:19 -0400

    Cambios Javi

--- static/js/slider.js
+++ static/js/slider.js
@@ -2,13 +2,38 @@ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  let i;
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("visible");
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].classList.add("visible");
+    setTimeout(showSlides, 2000);
+}
+
+document.getElementById('flecha-izquierda').addEventListener('click', function() {
+  changeSlide(-1);
+});
+
+document.getElementById('flecha-derecha').addEventListener('click', function() {
+  changeSlide(1);
+});
+
+function changeSlide(n) {
   let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("visible");
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  slideIndex += n;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+  slides[slideIndex - 1].classList.add("visible");
 }
